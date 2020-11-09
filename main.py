7feea3596917e89fd971bc6724ec3dcbdd27cffa
Shay Acrich <me@shayacrich.com>
2020-11-09 12:05:03 +0200

    wip

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from numba import njit, prange, float64, int64
 import quantecon as qe
 import random
+from wage_distribution import lognormal_draws
 
 
 """
@@ -52,15 +53,15 @@ def lognormal_draws(n=100, μ=2.5, σ=1.4, seed=None):
         seed = random.randint(1000,10001)
     np.random.seed(seed)
     z = np.random.randn(n)
-    w_draws = np.exp(μ + σ * z) * 2
-    return w_draws
+    draws = np.exp(μ + σ * z) * 2
+    return draws
 
 
 @njit
 def update_d(h):
     d = np.empty_like(a_grid)
     for i, a in enumerate(a_grid):
-        hf = lambda x: interp(w_grid, h[i], x)
+        hf = lambda x: interp(w_grid, h[i, :], x)
         d[i] = np.mean(hf(w_draws))
     for i in range(len(d)):
         if np.isnan(d[i]):
@@ -91,16 +92,17 @@ def update_v(v, h, d):
                 rhs = u(consumption[:z]) + β*((1 - α)*v[:, j][:z] + α*d[:z])
                 v_new[i, j] = np.nanmax(rhs)
                 if np.isnan(v_new[i, j]):
+                    # @TODO is nan then raise, don't assign nan above, using VERY_SMALL_NUMBER instead.
                     a_opt_employed[i, j] = 0
                 else:
                     a_opt_employed[i, j] = np.where(rhs == v_new[i, j])[0][0]
 
-            if a_opt_employed[i, j] > i:
+            #if a_opt_employed[i, j] > i:
                 # household wants to increase savings, let's tax it.
                 #print(i)
                 #print("a_opt before taxes:")
                 #print(a_opt_employed[i, j])
-                a_opt_employed[i, j] -= round(τ * (a_opt_employed[i, j] - i))
+            #    a_opt_employed[i, j] -= round(τ * (a_opt_employed[i, j] - i))
                 #print("a_opt after taxes:")
                 #print(a_opt_employed[i, j])
 
@@ -192,57 +194,126 @@ def find_nearest_index(array, value):
     return (np.abs(array - value)).argmin()
 
 
-def draw_lifetime(T=100, a_0_value=1):
-    stays_employed = binomial_draws(n=T)
-    u_t = np.empty(T)
-    is_employed = 1
-    seed = random.randint(1000,10001)
-    w = lognormal_draws(n=T, μ=0.8, σ=0.7, seed=seed)
-    employment_spells = np.empty(T)
-    w_t = w[0]
-    realized_wage = np.empty(T)
-    separations = []
-    a_0 = a_grid[find_nearest_index(a_grid, a_0_value)]
-    a = np.empty(T + 1)
-    a[0] = a_0
-    consumption = np.empty(T)
-    itu = []
-    reservation_wage = np.empty(T)
-    for t in range(T):
-        employment_spells[t] = is_employed
-        w_index = find_nearest_index(w_grid, w_t)
-        a_index = np.where(np.isclose(a_grid, a[t]))[0][0]
+def generate_lifetime(T=100, a_0=1):
+    """
+    Given initial asset level a_0, this function returns employment,
+    consumption and savings decisions and separations and wage incidences
+    for an agent living T periods.
+    """
 
+    # T binomial draws that determine for every period t whether a separation occurs
+    # we only look at these draws in periods where is_employed=1.
+    is_separated_at = binomial_draws(n=T, α=α)
+
+    # lists periods in which separations occured.
+    # a separation would occur in period t if is_employed=1 and is_separated_at[t] = 1.
+    separations = []
+
+    # agents always start employed, and earn their initial wage up to the first separation event.
+    # this variable holds the employment status of the agent.
+    # this is a variable and not a vector - it only states the current employment status.
+    # this variable acts like a boolean, but we use {0,1} instead for convenience (this is what
+    # comes out of is_separated_at, and what we store in employment_spells).
+    is_employed = 1
+
+    # a verctor of length T that states whether the agent was employed or not at every t.
+    # if an agent is employed at period t then employment_spells[t] = 1, otherwise 0.
+    # we use {0,1} to be able to sketch these results on a line chart, and also for
+    # confortably doing vector multiplication with the wage vector, so as to attain a sum
+    # of income in all periods.
+    employment_spells = np.empty(T)
+
+    # offered wage at period t (vector of size T).
+    # this vector is only used if the agent is currenly unemployed.
+    # this is the actual wage and not a grid index, and the wage drawn may not even
+    # be on the grid, so we approximate using find_nearest_index wherever necessary.
+    offered_wage_at = lognormal_draws(n=T, μ=μ, σ=σ)
+
+    # wage of employed worker, or offered wage for the unemployed.
+    # this is a variable and not a vector - it only states the current wage.
+    # this is the actual wage and not a grid index.
+    w_t = w_grid[0]
+
+    # a vector of size T that holds the offered/current wage for every t.
+    # these are the actual wage levels and not grid indices.
+    # realized wages may not always fit on the grid, as their taken from
+    # draws on a lognormal distribution.
+    realized_wage = np.empty(T)
+
+    # this is the minimal offered wage that will be accepted.
+    # it changes with assets. an agent with a high level of assets may choose
+    # to eat them away until a higher wage offer arives, while a poorer agent
+    # may choose to accept job offers even if the offered wage is low.
+    # this is the actual wage and not a grid index, but the wages are always
+    # on the grid, meaning that the real reservation wage, if calculated, may be
+    # slightly different than the one represented in this vector, but for all
+    # computational purposes this shouldn't make a difference.
+    reservation_wage = np.empty(T)
+
+    # level of assets (savings decision) at period t.
+    # this has T+1 values because, although we only draw T periods,
+    # there's still a savings decision made at period T for period T+1.
+    # the value at t=0 is the initial assets of the agent.
+    # these initial assets are the source of wealth heterogeneity in the model.
+    # this vector holds the actual asset levels and not their index on the grid.
+    a = np.empty(T + 1)
+    a[0] = a_grid[find_nearest_index(a_grid, a_0)]
+
+    # a vector of size T, indicating consumption at period t.
+    # consumption represents extra consumption above the minimal level requried
+    # by every agent at every period. consumption is everything earned or stored
+    # as assets that's not saved as next period assets, so the equation is:
+    # consumption - minimal_consumption + next_period_assets = wage_or_unemployment_benefits + current_period_assets * (1 + r)
+    consumption = np.empty(T)
+
+    # vector of size T holding the single period t utility from consumption.
+    # this is equivalent to np.log(consumption).
+    # consumption may be very small, so the utility may be negative, but consumption
+    # should never be negative, so utility should always be defined.
+    u_t = np.empty(T)
+
+    for t in range(T):
+        w_index = find_nearest_index(w_grid, w_t)
+        a_index = find_nearest_index(a_grid, a[t])
+
+        employment_spells[t] = is_employed
         reservation_wage[t] = w_grid[np.where(accept_or_reject[a_index, :] == 1)[0][0]]
 
         if is_employed:
             a[t+1] = a_grid[a_opt_employed[a_index, w_index]]
-            consumption[t] = w_t + a[t] - a[t+1]
-            u_t[t] = u(consumption[t])
-            is_employed = stays_employed[t]
+            consumption[t] = w_t + a[t] - a[t+1] - minimal_consumption
+            is_employed = is_separated_at[t]
             if not is_employed:
                 separations.append(t)
 
         else:
-            w_t = w[t]
+            w_t = offered_wage_at[t]
             w_index = find_nearest_index(w_grid, w_t)
             is_employed = accept_or_reject[a_index, w_index]
             if is_employed:
                 a[t+1] = a_grid[a_opt_employed[a_index, w_index]]
-                consumption[t] = w_t + a[t] - a[t+1]
-                u_t[t] = u(consumption[t])
+                consumption[t] = w_t + a[t] - a[t+1] - minimal_consumption
             else:
                 a[t+1] = a_grid[a_opt_unemployed[a_index, w_index]]
-                consumption[t] = c + a[t] - a[t+1]
-                u_t[t] = u(consumption[t])
+                consumption[t] = c + a[t] - a[t+1] - minimal_consumption
+        u_t[t] = u(consumption[t])
         realized_wage[t] = w_t
-        realized_w_index = find_nearest_index(w_grid, w_t)
-        itu.append((v[a_index, realized_w_index], h[a_index, realized_w_index]))
+
+    # drop assets at period T+1 because our graphs only go up to T.
     a = a[:-1].copy()
-    return a, u_t, realized_wage, employment_spells, itu, consumption, separations, reservation_wage
+
+    return (
+        a,
+        u_t,
+        realized_wage,
+        employment_spells,
+        consumption,
+        separations,
+        reservation_wage
+    )
 
 
-def draw(a, u_t, realized_wage, employment_spells, itu, consumption, separations, reservation_wage, T=100):
+def draw(a, u_t, realized_wage, employment_spells, consumption, separations, reservation_wage, T=100):
     fig, ax = plt.subplots()
     ax.set_xlabel('periods')
     ax.set_ylabel('stuff')
@@ -261,25 +332,31 @@ def draw(a, u_t, realized_wage, employment_spells, itu, consumption, separations
 
 def run_a_lot(T):
     wages = []
+    assets = []
     for i in range(1000):
-        a, u_t, realized_wage, employment_spells, itu, consumption, separations, reservation_wage = draw_lifetime(T=T, a_0_value=0)
+        a, u_t, realized_wage, employment_spells, consumption, separations, reservation_wage = generate_lifetime(T=T, a_0=0)
         wages.append(np.dot(realized_wage,employment_spells)/np.sum(employment_spells))
+        assets.append(a[T-1])
     print("average lifetime wage for the poor: {}".format(np.mean(np.asarray(wages))))
+    print("assets at end of life for the poor: {}".format(np.mean(assets)))
     wages = []
+    assets = []
     for i in range(1000):
-        a, u_t, realized_wage, employment_spells, itu, consumption, separations, reservation_wage = draw_lifetime(T=T, a_0_value=150)
+        a, u_t, realized_wage, employment_spells, consumption, separations, reservation_wage = generate_lifetime(T=T, a_0=150)
         wages.append(np.dot(realized_wage,employment_spells)/np.sum(employment_spells))
+        assets.append(a[T-1])
     print("average lifetime wage for the rich: {}".format(np.mean(np.asarray(wages))))
+    print("assets at end of life for the rich: {}".format(np.mean(assets)))
 
 def run(T):
-    a, u_t, realized_wage, employment_spells, itu, consumption, separations, reservation_wage = draw_lifetime(T=T, a_0_value=0)
-    draw(a, u_t, realized_wage, employment_spells, itu, consumption, separations, reservation_wage, T=T)
+    a, u_t, realized_wage, employment_spells, consumption, separations, reservation_wage = generate_lifetime(T=T, a_0=0)
+    draw(a, u_t, realized_wage, employment_spells, consumption, separations, reservation_wage, T=T)
     print(np.sum(employment_spells))
     print(np.dot(realized_wage,employment_spells)/np.sum(employment_spells))
-    a, u_t, realized_wage, employment_spells, itu, consumption, separations, reservation_wage = draw_lifetime(T=T, a_0_value=150)
+    a, u_t, realized_wage, employment_spells, consumption, separations, reservation_wage = generate_lifetime(T=T, a_0=150)
     print(np.sum(employment_spells))
     print(np.dot(realized_wage,employment_spells)/np.sum(employment_spells))
-    draw(a, u_t, realized_wage, employment_spells, itu, consumption, separations, reservation_wage, T=T)
+    draw(a, u_t, realized_wage, employment_spells, consumption, separations, reservation_wage, T=T)
 
 
 c = 2
@@ -287,15 +364,18 @@ c = 2
 
 # see: https://www.bls.gov/news.release/pdf/nlsoy.pdf
 T = 408
-α = 1/34
+α = 1/70
 
 τ = 0.8
 
-w_draws = lognormal_draws(n=1000, μ=0.8, σ=0.7, seed=1234)
+μ=0.2
+σ=1.2
+
+w_draws = lognormal_draws(n=1000, μ=μ, σ=σ, seed=1234)
 
 w_min = 1e-10
 w_max = np.max(w_draws)
-w_size = 50
+w_size = 100
 
 print("wage grid is from {min} to {max}, with size {size}, and the average wage is {avg}".format(
     min=w_min,
@@ -310,7 +390,7 @@ a_max = 200
 a_size = 100
 a_grid = np.linspace(a_min, a_max, a_size)
 
-minimal_consumption = 3
+minimal_consumption = 0
 
 
 ism = 0.98  # inter-temporal savings motive
@@ -335,4 +415,4 @@ r = (ism/β) - 1
 
 v, h, accept_or_reject, a_opt_unemployed, a_opt_employed = solve_model()
 
-run_a_lot(T)
+run(T)
--- wage_distribution.py
+++ wage_distribution.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from scipy.stats import bernoulli
 
 N_points = 4000
 
+μ=0.8
+σ=0.7
 
 def lognormal_draws(n=100, μ=1.5, σ=1.4, seed=1234):
     np.random.seed(seed)
@@ -12,16 +14,25 @@ def lognormal_draws(n=100, μ=1.5, σ=1.4, seed=1234):
     return w_draws
 
 
-w = lognormal_draws(n=N_points, μ=0.8, σ=0.7)
-average = np.mean(w)
-print(average)
-bins = np.linspace(0, 400, 50)
+def bimodal_draws(n=100, μ=1.5, σ=1.4, seed=1234):
+    mu, sigma = 1, 0.7 # mean and standard deviation
+    low_w_draws = np.random.normal(mu, sigma, n)
+    low_w_draws = lognormal_draws(n=n, μ=0.5, σ=0.2, seed=seed)
+    mu, sigma = 50, 1.7 # mean and standard deviation
+    high_w_draws = np.random.normal(mu, sigma, n)
+    α = 0.05
+    draws = bernoulli.rvs(1 - α, size=n)
+    w_draws = (1-draws)*high_w_draws + draws*low_w_draws
+    return w_draws
 
-fig,ax = plt.subplots(1,1)
-ax.hist(w, bins = bins)
-plt.axvline(x=average)
-ax.set_title("histogram of wage distribution")
-ax.set_xticks(bins)
-ax.set_xlabel('wage')
-ax.set_ylabel('density')
-plt.show()
+
+if __name__ == '__main__':
+    w = bimodal_draws(n=N_points, μ=μ, σ=σ)
+    average = np.mean(w)
+    print(np.min(w))
+    print(average)
+    print(np.median(w))
+    print(np.max(w))
+
+    count, bins, ignored = plt.hist(w, 100, density=True)
+    plt.show()
